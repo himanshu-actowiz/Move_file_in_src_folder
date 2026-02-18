a06d88f759a0f67e432c2710541dff060cf61ccf
Hemanshu Marwadi <Hemanshu.Marwadi@act.local>
2026-02-18 10:19:58 +0530

    Move file Source folder to Destination folder & the remove any 2 file for Destination folder

---
+++ 2027_folder's.py
@@ -0,0 +1,22 @@
+import os
+from datetime import datetime, timedelta
+from calendar import isleap
+
+Current_date = input("Enter a CURRENT DATE : YYYY-MM-DD ")
+
+def create_folders_between_dates(Current_date):
+    Convert_date_DATETIME = datetime.strptime(Current_date, "%Y-%m-%d")
+    Current_Day_Number = Convert_date_DATETIME.timetuple().tm_yday
+    year = Convert_date_DATETIME.year # Get year correctly
+    total_days = 366 if isleap(year) else 365  # Check leap year properly
+    
+    Find_Remaining_Days = total_days - Current_Day_Number
+
+    for i in range(1, Find_Remaining_Days + 1):
+        Increase_Date = Convert_date_DATETIME + timedelta(days=i)
+        s = Increase_Date.strftime("%Y-%m-%d")
+        
+        os.mkdir(f"C:/Users/hemanshu.marwadi/Desktop/2027_year_folder/{s}")
+        
+
+create_folders_between_dates(Current_date)
---
+++ move_files.py
@@ -0,0 +1,39 @@
+
+import os
+
+source_path = "C:/Users/hemanshu.marwadi/Desktop/OS_MAKE_DIR"
+destination_path = "C:/Users/hemanshu.marwadi/Desktop/2027_year_folder"
+
+def move_file(src_path,dst_path):
+
+    for folder in os.listdir(src_path):# folder
+        # if folder == '2026-02-22':
+        src_folder_path = os.path.join(src_path,folder)
+        year_change = folder.replace("2026", "2027")
+        print(year_change)
+        print(src_path)
+        print(folder)
+        print(src_folder_path)
+        dest_folder=os.path.join(dst_path,year_change)
+
+        for src_folder_file in os.listdir(src_folder_path):# files
+            print(src_folder_file,src_folder_path)
+            src_file_path = os.path.join(src_folder_path,src_folder_file)
+            dest_file_path=os.path.join(dest_folder,src_folder_file)
+
+            os.rename(src_file_path,dest_file_path)
+            
+def remove_file(destination_path):
+    for i in os.listdir(destination_path):
+        remove_file_path = os.path.join(destination_path,i)
+        for j in os.listdir(remove_file_path):
+            remove_path = os.path.join(remove_file_path,j)
+            
+            if((".py" and ".java") in remove_path):
+                os.remove(remove_path)
+    
+            
+move_folder_file = move_file(source_path,destination_path)
+remove_file(destination_path)
+
+
